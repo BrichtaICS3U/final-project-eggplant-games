c10ec69f4c6ef4b3c83146d7f512af87ea16a5d6
Nako1 <nspro1@ocdsb.ca>
2018-05-06 21:48:21 -0400

    menu looks ass

--- Game.py
+++ Game.py
@@ -26,7 +26,7 @@ clock = pygame.time.Clock()
 def Button(msg,x,y,w,h,col1,col2,FS,action = None,CT = None):
     """
         this is a short function that acts as a button class (will likley be changed into a module)
-        msg    --- the text that will appear on the button
+        msg    --- the label that will appear on the button
         x      --- the x coordinate for the button
         y      --- the y coordinate for the button
         w      --- the width od the button
@@ -35,36 +35,36 @@ def Button(msg,x,y,w,h,col1,col2,FS,action = None,CT = None):
         col2   --- the second colour of the button
         FS     --- the font size of the buttons text
         action --- the function that the button activates
-        CT     --- the controls text (only for layer 3/controls screen)
+        CT     --- the controls text (only for layer 3/controls screen Buttons)
     """
     
-    global layer
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
+    global layer                        #adds the layer variable to the Button function
+    mouse = pygame.mouse.get_pos()      #gathers the position of the mouse
+    click = pygame.mouse.get_pressed()  #finds out if the mouse has been pressed
 
-    if x < mouse[0] <x+w and y < mouse[1] < y+h and click[0] == 1:
-        pygame.draw.rect(screen,BLACK,(x,y,w,h))
-        if click[0] == 1 and action != None:
-            action()
-    elif x < mouse[0] < x+w and y < mouse[1] < y+h and layer != 3:
-        pygame.draw.rect(screen,col2,(x,y,w,h))
-    elif x < mouse[0] < x+w and y < mouse[1] < y+h and layer == 3:
-        pygame.draw.rect(screen,col2,(x,y,w,h))
+    if x < mouse[0] <x+w and y < mouse[1] < y+h and click[0] == 1:  #if the cursor is over the button and mouse 1 has been pressed
+        pygame.draw.rect(screen,BLACK,(x,y,w,h))                    #redraw the Button but BLACK
+        if click[0] == 1 and action != None:                        #if the Button has a function assigned to it
+            action()                                                #activate that function
+    elif x < mouse[0] < x+w and y < mouse[1] < y+h and layer != 3:  #if the cursor is over the button but mouse 1 hasn't been pressed yet
+        pygame.draw.rect(screen,col2,(x,y,w,h))                     #redraw the Button but in a diffirent colour 
+    elif x < mouse[0] < x+w and y < mouse[1] < y+h and layer == 3:  #this if statement is for only the Buttons located in the "Controls" section of the menu
+        pygame.draw.rect(screen,col2,(x,y,w,h))                     #it redraws the button in its second colour. 
         
-        CFont = pygame.font.Font("freesansbold.ttf",FS)
-        CText = CFont.render(CT,True,WHITE)
-        CTextrect = CText.get_rect()
-        CTextrect.center = (1000, y + (h/2))
-        screen.blit(CText,CTextrect)
+        CFont = pygame.font.Font("freesansbold.ttf",FS)     #as well as displays a definition of what that Button does in the game
+        CText = CFont.render(CT,True,WHITE)                 #
+        CTextrect = CText.get_rect()                        #
+        CTextrect.center = (1000, y + (h/2))                #
+        screen.blit(CText,CTextrect)                        #
         
-    else:
-        pygame.draw.rect(screen,col1,(x,y,w,h))
+    else:                                                           #if nothing is interacting with the Button 
+        pygame.draw.rect(screen,col1,(x,y,w,h))                     #simply draw it with its orginal perameters
 
-    Bfont = pygame.font.Font("freesansbold.ttf",FS)
-    Btext = Bfont.render(msg,True,BLACK)
-    Btextrect = Btext.get_rect()
-    Btextrect.center = (x + (w/2), y + (h/2))
-    screen.blit(Btext,Btextrect)
+    Bfont = pygame.font.Font("freesansbold.ttf",FS)                 #these few lines are the Button labels 
+    Btext = Bfont.render(msg,True,BLACK)                            #
+    Btextrect = Btext.get_rect()                                    #
+    Btextrect.center = (x + (w/2), y + (h/2))                       #
+    screen.blit(Btext,Btextrect)                                    #
     
 def TEXT(TXT,x,y,TF):
     """this function exists to create headings for each menu screens"""
@@ -121,51 +121,51 @@ while Game:
 
     if layer == 1:
         #title 
-        TEXT("Ctrl and Destroy",300,50,70)
+        TEXT("Ctrl and Destroy",300,50,70)                          #Game title
 
-        Button("Start", 20,200,210,80,RED,GREEN,35,Game_intro)
-        Button("Controls",20,340,210,80,RED,GREEN,35,Controls)
-        Button("Settings",20,480,210,80,RED,GREEN,35,Settings)
-        Button("Quit",20,620,210,80,RED,GREEN,35,Quit)
+        Button("Start", 20,200,210,80,RED,GREEN,35,Game_intro)      #this Button leads to the game intro, and then into the game
+        Button("Controls",20,340,210,80,RED,GREEN,35,Controls)      #this Button leads to the Controls menu, which quickly teaches the player the controls
+        Button("Settings",20,480,210,80,RED,GREEN,35,Settings)      #this Button leads to the Settings menu, which allows the player to either turn off the music or select a specific game difficulty 
+        Button("Quit",20,620,210,80,RED,GREEN,35,Quit)              #this Button simply quits the game
         
     elif layer == 2:
         #Settings
-        TEXT("Settings",180,50,70)
-        TEXT("Music",SCREEN_WIDTH/2,175,50)
-        TEXT("Difficulty",SCREEN_WIDTH/2,450,50)
+        TEXT("Settings",180,50,70)                                          #settings Heading
+        TEXT("Music",SCREEN_WIDTH/2,175,50)                                 #Music Sub-Heading
+        TEXT("Difficulty",SCREEN_WIDTH/2,450,50)                            #Difficulty Sub-Heading
 
-        Button("ON",SCREEN_WIDTH/3,250,100,65,RED,GREEN,35,None)
-        Button("OFF",SCREEN_WIDTH/1.75 + 20 ,250,100,65,RED,GREEN,35,None)
+        Button("ON",SCREEN_WIDTH/3,250,100,65,RED,GREEN,35,None)            #this Button toggles the music on 
+        Button("OFF",SCREEN_WIDTH/1.75 + 20 ,250,100,65,RED,GREEN,35,None)  #this Button toggles the music off
 
-        Button("Baby",SCREEN_WIDTH/6 ,525,220,65,RED,GREEN,35,None)
-        Button("Boring",525,525,220,65,RED,GREEN,35,None)
-        Button("Thrilling",842 ,525,220,65,RED,GREEN,35,None)
+        Button("Baby",SCREEN_WIDTH/6 ,525,220,65,RED,GREEN,35,None)         #this Button is used to toggle the easiest difficulty    
+        Button("Boring",525,525,220,65,RED,GREEN,35,None)                   #this Button is used to toggle the medium difficulty 
+        Button("Thrilling",842 ,525,220,65,RED,GREEN,35,None)               #this Button is used to toggle the hardest difficulty
         
-        Button("Back",20,700,80,50,RED,GREEN,25,Menu)
+        Button("Back",20,700,80,50,RED,GREEN,25,Menu)                       #this Button will return the user to the main menu
 
     elif layer == 3:
         #Controls
-        TEXT("Game Controls",300,50,70)
+        TEXT("Game Controls",300,50,70)                             #Controls Heading
         
         #move controls
-        Button("W",120,100,50,50,RED,GREEN,35,None,"Move UP")
-        Button("S",120,170,50,50,RED,GREEN,35,None,"Move DOWN")
-        Button("A",50,170,50,50,RED,GREEN,35,None,"Move RIGHT")
-        Button("D",190,170,50,50,RED,GREEN,35,None,"Move LEFT")
+        Button("W",120,100,50,50,RED,GREEN,35,None,"Move UP")       #hovering over this button will tell the player how to move up
+        Button("S",120,170,50,50,RED,GREEN,35,None,"Move DOWN")     #hovering over this button will tell the player how to move down
+        Button("A",50,170,50,50,RED,GREEN,35,None,"Move RIGHT")     #hovering over this button will tell the player how to move left
+        Button("D",190,170,50,50,RED,GREEN,35,None,"Move LEFT")     #hovering over this button will tell the player how to move Right
 
         #melee atk control
-        Button("E",120,300,50,50,RED,GREEN,35,None,"Melee Atk")
+        Button("E",120,300,50,50,RED,GREEN,35,None,"Melee Atk")     #hovering over this button will tell the player how to Attack 
 
         #shoot controls
-        Button("",120,430,50,50,RED,GREEN,35,None,"Shoot UP")
-        Button("",120,500,50,50,RED,GREEN,35,None,"Shoot DOWN")
-        Button("",50,500,50,50,RED,GREEN,35,None,"Shoot LEFT")
-        Button("",190,500,50,50,RED,GREEN,35,None,"Shoot RIGHT")
+        Button("",120,430,50,50,RED,GREEN,35,None,"Shoot UP")       #hovering over this button will tell the player how to shoot up  
+        Button("",120,500,50,50,RED,GREEN,35,None,"Shoot DOWN")     #hovering over this button will tell the player how to shoot down
+        Button("",50,500,50,50,RED,GREEN,35,None,"Shoot LEFT")      #hovering over this button will tell the player how to shoot left
+        Button("",190,500,50,50,RED,GREEN,35,None,"Shoot RIGHT")    #hovering over this button will tell the player how to shoot right
 
         #pause menu bind
-        Button("ESC",100,630,90,50,RED,GREEN,35,None,"Toggle Menu")
+        Button("ESC",100,630,90,50,RED,GREEN,35,None,"Toggle Menu") #hovering over this button will tell the player how to toggle the main menu
         
-        Button("Back",20,700,80,50,RED,GREEN,25,Menu,None)
+        Button("Back",20,700,80,50,RED,GREEN,25,Menu,None)          #this Button will return the user to the main menu
 
     elif layer == 4:
         #game intro
