b1550a9b43003a802319c88c5092c3fccb9b1350
Abbey Jayne <ajayn1@ocdsb.ca>
2018-05-13 19:44:53 -0400

    rip

--- HeroPractice.py
+++ HeroPractice.py
@@ -3,6 +3,7 @@ import pygame, random
 #Import classes
 from hero import Hero, Enemy, Bullet
 from Door import DOOR
+from LVLs import LVL
 
 pygame.init()
 
@@ -15,6 +16,9 @@ M_TEXT = (164,44,214)
         
 SCREEN_WIDTH=1250
 SCREEN_HEIGHT=800
+
+Y = 1
+X = 1
 
 def TEXT(TXT,x,y,TF):
     """this function exists to create headings for each menu screens"""
@@ -40,6 +44,26 @@ def NEXT_SCREEN_LEFT():
 def NEXT_SCREEN_DOWN():
     """this function will transfer the player to a new screen UP and reset their position in the correct area"""
     player.rect.y = 6
+
+def Change_SCREEN():
+    global Y
+    global X
+    if player.rect.y < 50:
+        player.rect.y = SCREEN_HEIGHT - 46
+        print("screen UP.")
+        Y += 1
+    elif player.rect.y > 750:
+        player.rect.y = 6
+        print("screen DOWN.")
+        Y -= 1
+    elif player.rect.x < 50:
+        player.rect.x = SCREEN_WIDTH - 36
+        print("screen LEFT.")
+        X -= 1
+    elif player.rect.x > 1150:
+        player.rect.x = 6
+        print("screen RIGHT.")
+        X += 1
  
 size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 screen = pygame.display.set_mode(size)
@@ -64,6 +88,26 @@ enemy = Enemy(BLACK, 40, 40)
 enemy_list.add(enemy)
 
 bullet = Bullet(BLACK, 5, 5, player.rect.x, player.rect.y)
+
+#list of lvls
+lvls = []
+
+#lvl 1-1
+lvl1 = LVL(3)
+lvls.append(lvl1)
+
+#lvl 1-2
+lvl2 = LVL(9)
+lvls.append(lvl2)
+
+#lvl 2-1
+lvl3 = LVL(5)
+lvls.append(lvl3)
+
+#lvl 2-2
+lvl4 = LVL(10)
+lvls.append(lvl4)
+
 
 #this is where all the "door" objects are created
 #top door
@@ -107,6 +151,27 @@ while carryOn:
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 carryOn=False
+
+        if Y == 1 and X == 1:
+            lvl1.draw(screen)
+            pygame.draw.rect(screen,RED,[50,50,50,50])
+            
+        if Y == 2 and X == 1:
+            lvl2.draw(screen)
+            pygame.draw.rect(screen,GREEN,[50,50,50,50])
+
+        if Y == 1 and X == 2:
+            lvl3.draw(screen)
+            pygame.draw.rect(screen,BLUE,[50,50,50,50])
+
+        if Y == 2 and X == 2:
+            lvl4.draw(screen)
+            pygame.draw.rect(screen,BLACK,[50,50,50,50])
+            
+        for lvl in lvls:
+            Door_collision_list = pygame.sprite.spritecollide(player,lvl.doors_list,False)       
+            for door in Door_collision_list:
+                Change_SCREEN()
          
         keys = pygame.key.get_pressed()
 
